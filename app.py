eadbf37f367ffa51ba9a0ff17906e47f63d11ad3
Li Wenkang <51111762+eye1218@users.noreply.github.com>
2024-03-16 16:00:26 +0800

    refactor: 更改端口

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from api.router.parser import parser_router
 
 app = FastAPI()
 
-app.include_router(parser_router, prefix="/parser")
+app.include_router(parser_router, prefix="/parser", tags=["解析数据"])
 
 if __name__ == "__main__":
-    run("app:app", host="0.0.0.0", port=8080, reload=True, log_level="debug")
+    run("app:app", host="0.0.0.0", port=32180, reload=True, log_level="debug")
