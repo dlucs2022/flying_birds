678d38e46d00ca8528e3abcd253ebfcf391bd967
Li Wenkang <51111762+eye1218@users.noreply.github.com>
2024-03-16 16:06:28 +0800

    refactor: 更改端口

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from uvicorn import run
 from fastapi import FastAPI
+from fastapi.routing import APIRouter
 
 from api.router.parser import parser_router
 
 
 app = FastAPI()
-
-app.include_router(parser_router, prefix="/parser", tags=["解析数据"])
+app.include_router(parser_router, prefix="/api/parser", tags=["解析数据"])
 
 if __name__ == "__main__":
-    run("app:app", host="0.0.0.0", port=32180, reload=True, log_level="debug")
+    run("app:app", host="127.0.0.1", port=32180, reload=True, log_level="debug")
